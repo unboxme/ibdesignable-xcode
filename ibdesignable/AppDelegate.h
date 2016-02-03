338b57ddab5d65473a9af323942eb1f40b92436f
unboxme <puzyrev.pavel@yahoo.com>
2016-02-04 00:04:31 +0300

    initial commit

---
+++ ibdesignable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ibdesignable
+//
+//  Created by Puzyrev Pavel on 03.02.16.
+//  Copyright © 2016 Puzyrev Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ibdesignable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ibdesignable
+//
+//  Created by Puzyrev Pavel on 03.02.16.
+//  Copyright © 2016 Puzyrev Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
